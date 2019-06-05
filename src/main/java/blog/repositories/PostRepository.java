d8f559ed3846086378a88126a2e0f54b5c16e02d
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-06-05 15:46:32 +0600

    Created UserRepository and PostRepository(Spring Data JPA)

---
+++ src/main/java/blog/repositories/PostRepository.java
@@ -0,0 +1,15 @@
+package blog.repositories;
+
+import blog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query("select p from Post p left join fetch p.author order by p.date desc")
+    List<Post> findLatest5Posts(Pageable pageable);
+}
