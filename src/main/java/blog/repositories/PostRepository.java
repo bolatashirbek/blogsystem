a47b4699700b865ae208237409d22dd4b2ef71df
Bolat Ashirbek <bolat.89arys@mail.ru>
2019-06-05 16:05:12 +0600

    Implemented the PostService to use the DB(JPA Repository)

--- src/main/java/blog/repositories/PostRepository.java
+++ src/main/java/blog/repositories/PostRepository.java
@@ -1,11 +1,11 @@
 package blog.repositories;
 
 import blog.models.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
